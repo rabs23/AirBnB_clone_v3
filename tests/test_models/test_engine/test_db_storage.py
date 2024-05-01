b18fd7fc88002f1a5f48cd72d71132e920987fa0
rabs23 <ruthrabs22@gmail.com>
2024-05-01 10:53:01 -0700

    db_test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -87,6 +87,7 @@ class TestFileStorage(unittest.TestCase):
     def test_save(self):
         """Test that save properly saves objects to file.json"""
 
+
 class TestDBStorage(unittest.TestCase):
     """Test the DBStorage class"""
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
@@ -165,7 +166,7 @@ class TestDBStorage(unittest.TestCase):
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_count(self):
-        """Test that count properly counts the number of objects in the database"""
+        """Test that  properly counts the number of objects in the database"""
         storage = DBStorage()
         state = State(name="California")
         state.save()
